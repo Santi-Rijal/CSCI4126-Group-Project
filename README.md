4ecc076bd97f07ce70fe2862e4b3d597b620e269
Adam Sarty <ad883184@dal.ca>
2023-11-27 02:05:40 -0400

    Included test execution file.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CSCI4126-Group-Project
+
+https://www.freepik.com/icon/wind_192756
+
+https://fonts.google.com/specimen/Roboto
+
+https://weatherstack.com
+
+https://pixabay.com/gifs/cloud-sky-blue-6619/
+
+https://dribbble.com/shots/6193512-Weather-Icon-Set-Rain
--- Server/Assets/Scripts/WeatherAPI.cs
+++ Server/Assets/Scripts/WeatherAPI.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System.Collections.Generic;
 
 public class WeatherAPIScript : MonoBehaviour
 {
@@ -8,13 +9,48 @@ public class WeatherAPIScript : MonoBehaviour
     private string apiKey = "bd08346cc82a086fd4a9861b555d1b89";
     private string apiUrl = "http://api.weatherstack.com/current";
     public UnityEngine.UI.Text temperatureText;
+    public UnityEngine.UI.Text windSpeedText; // UI Text for wind speed
+
+    public GameObject hailObject, rainyObject, snowyObject, sunnyCloudsObject, thunderObject;
+    private Dictionary<string, GameObject> weatherAnimations;
+
+    public float updateInterval = 1800f; // Time in seconds for each weather update (e.g., 1800 seconds = 30 minutes)
+
+    private float timer;
 
     void Start()
     {
-        StartCoroutine(GetTemperature());
+        InitializeWeatherAnimations();
+        StartCoroutine(GetWeatherData());
+        timer = updateInterval;
     }
 
-    IEnumerator GetTemperature()
+    void Update()
+    {
+        // Update timer
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            StartCoroutine(GetWeatherData());
+            timer = updateInterval;
+        }
+    }
+
+    void InitializeWeatherAnimations()
+    {
+        weatherAnimations = new Dictionary<string, GameObject>
+        {
+            { "Hail", hailObject },
+            { "Rain", rainyObject },
+            { "Snow", snowyObject },
+            { "Sunny", sunnyCloudsObject },
+            { "Thunderstorm", thunderObject }
+        };
+
+        DeactivateAllAnimations();
+    }
+
+    IEnumerator GetWeatherData()
     {
         string fullUrl = $"{apiUrl}?access_key={apiKey}&query={city}";
         UnityWebRequest request = UnityWebRequest.Get(fullUrl);
@@ -30,8 +66,38 @@ public class WeatherAPIScript : MonoBehaviour
             string jsonResult = System.Text.Encoding.UTF8.GetString(request.downloadHandler.data);
             WeatherData weatherData = JsonUtility.FromJson<WeatherData>(jsonResult);
 
-            // Set the temperature text
+            // Update UI elements
             temperatureText.text = weatherData.current.temperature + "°C";
+            windSpeedText.text = weatherData.current.wind_speed + " KM/H";
+
+            // Trigger the correct animation
+            TriggerWeatherAnimation(weatherData.current.weather_descriptions[0]);
+        }
+    }
+
+    void TriggerWeatherAnimation(string weatherDescription)
+    {
+        // Deactivate all animations first
+        DeactivateAllAnimations();
+
+        // Determine which animation to play
+        if (weatherDescription.Contains("Hail") || weatherDescription.Contains("Sleet"))
+            weatherAnimations["Hail"].SetActive(true);
+        else if (weatherDescription.Contains("Rain") || weatherDescription.Contains("Drizzle") || weatherDescription.Contains("Light Rain") || weatherDescription.Contains("Heavy Rain") || weatherDescription.Contains("Fog") || weatherDescription.Contains("Mist"))
+            weatherAnimations["Rain"].SetActive(true);
+        else if (weatherDescription.Contains("Snow") || weatherDescription.Contains("Light Snow") || weatherDescription.Contains("Heavy Snow"))
+            weatherAnimations["Snow"].SetActive(true);
+        else if (weatherDescription.Contains("Sunny") || weatherDescription.Contains("Clear") || weatherDescription.Contains("Partly Cloudy") || weatherDescription.Contains("Cloudy") || weatherDescription.Contains("Overcast"))
+            weatherAnimations["Sunny"].SetActive(true);
+        else if (weatherDescription.Contains("Thunderstorm"))
+            weatherAnimations["Thunderstorm"].SetActive(true);
+    }
+
+    void DeactivateAllAnimations()
+    {
+        foreach (var anim in weatherAnimations.Values)
+        {
+            anim.SetActive(false);
         }
     }
 
@@ -45,5 +111,7 @@ public class WeatherAPIScript : MonoBehaviour
     public class Current
     {
         public int temperature;
+        public string[] weather_descriptions; // Array of weather descriptions
+        public int wind_speed; // Wind speed in km/h
     }
 }
